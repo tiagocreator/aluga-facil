9919e30a1ebc24c60a358128d446c6ee8872cf40
Tiago Leite <contato@tiagocreator.com>
2024-02-14 16:27:32 -0300

    Create banner component

---
+++ src/components/Banner.tsx
@@ -0,0 +1,20 @@
+const Banner: React.FC = () => {
+  return (
+    <>
+      <section className='banner-section'>
+        <div className='container'>
+          <div className='banner-content'>
+            <div className='banner-content__text'>
+              <h2>Carros para suas necessidades!</h2>
+              <p>
+                Melhorers marcas. Suporte <span>24/7</span>.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Banner;
