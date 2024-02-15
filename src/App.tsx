c3491dc3b3a3b9301ce1753294fd22d4d1f997f6
Tiago Leite <contato@tiagocreator.com>
2024-02-14 21:22:29 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+
+import { Models, TestimonialsPage, Team, Contact, Home, About } from './pages';
+import { Navbar } from './components';
+
+import '../src/dist/styles.css';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route index path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='models' element={<Models />} />
+        <Route path='testimonials' element={<TestimonialsPage />} />
+        <Route path='team' element={<Team />} />
+        <Route path='contact' element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
