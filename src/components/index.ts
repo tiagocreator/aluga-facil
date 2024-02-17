59ca79e0eb2d07b15e8bfdc7853c83fbd11e75fb
Tiago Leite <contato@tiagocreator.com>
2024-02-17 18:19:44 -0300

    Create default component exports

---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+export { default as Banner } from './Banner';
+export { default as BookCar } from './BookCar';
+export { default as CarBox } from './CarBox';
+export { default as ChooseUs } from './ChooseUs';
+export { default as Download } from './Download';
+export { default as Faq } from './Faq';
+export { default as Footer } from './Footer';
+export { default as Hero } from './Hero';
+export { default as HeroPages } from './HeroPages';
+export { default as Navbar } from './Navbar';
+export { default as PlanTrip } from './PlanTrip';
+export { default as PickCar } from './PickCar';
+export { default as Testimonials } from './Testimonials';
