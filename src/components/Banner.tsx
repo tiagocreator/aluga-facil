bb5e670395bb16b2ad2a969764904a1b5afade4f
Tiago Leite <contato@tiagocreator.com>
2024-02-24 21:50:36 -0300

    Fix typo

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,19 +1,17 @@
 const Banner: React.FC = () => {
   return (
-    <>
-      <section className='banner-section'>
-        <div className='container'>
-          <div className='banner-content'>
-            <div className='banner-content__text'>
-              <h2>Carros para suas necessidades!</h2>
-              <p>
-                Melhorers marcas. Suporte <span>24/7</span>.
-              </p>
-            </div>
+    <section className='banner-section'>
+      <div className='container'>
+        <div className='banner-content'>
+          <div className='banner-content__text'>
+            <h2>Carros para suas necessidades!</h2>
+            <p>
+              Melhorers marcas. Suporte <span>24/7</span>.
+            </p>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
