5f67578f0ad1cbdaaf99657d99f5bedf128c22a1
Tiago Leite <contato@tiagocreator.com>
2024-03-28 13:27:37 -0300

    Use fonts from fontsource

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,18 @@ import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import { Models, TestimonialsPage, Team, Contact, Home, About } from './pages';
 import { Navbar } from './components';
 
+import '@fontsource/poppins/300.css';
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/700.css';
+
+import '@fontsource/rubik/300.css';
+import '@fontsource/rubik/400.css';
+import '@fontsource/rubik/500.css';
+import '@fontsource/rubik/600.css';
+import '@fontsource/rubik/700.css';
+
 import '../src/dist/styles.css';
 
 const App: React.FC = () => {
